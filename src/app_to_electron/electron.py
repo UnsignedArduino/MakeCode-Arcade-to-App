17df1394b3c1408b733cce78c68b26619c740d37
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-05-13 16:44:48 +0000

    Copy static files to Electron source

--- src/app_to_electron/electron.py
+++ src/app_to_electron/electron.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from mkcd_to_app.config import Config
 from utils.cmd import run_shell_command
+from utils.filesystem import copy_these, delete_these
 from utils.logger import create_logger
 
 logger = create_logger(name=__name__, level=logging.INFO)
@@ -20,13 +21,17 @@ def generate_electron(config: Config, prj_name: str, dist_dir: Path, cwd: Path):
     """
     logger.debug(f"Creating Electron app for {prj_name}")
     # Initialize an Electron Forge project
-    if (cwd / prj_name).exists():
+    prj_dir = cwd / prj_name
+    prj_src_dir = prj_dir / "src"
+    if prj_dir.exists():
         logger.debug(f"Project {prj_name} already exists, continuing...")
     else:
         run_shell_command(f"npx --yes create-electron-app@latest {prj_name} --template=webpack", cwd=cwd)
+    # Delete index.html and index.css
+    delete_these(["index.html", "index.css"], prj_src_dir)
     # Start copying files
     old_dir = dist_dir
-    new_dir = cwd / prj_name / "src"
+    new_dir = prj_src_dir
     logger.debug(f"Copying HTML, CSS, and JS from {old_dir} to {new_dir}")
     # Copy all files in old_dir to new_dir
-    
+    copy_these(list([p.name for p in old_dir.glob("*")]), old_dir, new_dir)
--- src/utils/filesystem.py
+++ src/utils/filesystem.py
@@ -7,6 +7,28 @@ from .logger import create_logger
 logger = create_logger(name=__name__, level=logging.INFO)
 
 
+def copy_these(dirs_and_files: list[str], src_dir: Path, dest_dir: Path):
+    """
+    Copies the specified directories and files in the src_dir to the dest_dir.
+    
+    :param dirs_and_files: A list of file names to copy.
+    :param src_dir: The source directory from which to copy the files.
+    :param dest_dir: The destination directory to which the files will be copied.
+    """
+    logger.debug(f"Copying {len(dirs_and_files)} directories and files")
+    for file in dirs_and_files:
+        src_path = src_dir / file
+        dest_path = dest_dir / file
+        if src_path.exists():
+            logger.debug(f"Copying {src_path} to {dest_path}")
+            if src_path.is_dir():
+                shutil.copytree(src_path, dest_path, dirs_exist_ok=True)
+            else:
+                shutil.copy2(src_path, dest_path)
+        else:
+            logger.debug(f"{src_path} does not exist, skipping copy.")
+
+
 def delete_these(dirs_and_files: list[str], dir: Path):
     """
     Deletes the specified directories and files from the given directory.
@@ -23,3 +45,5 @@ def delete_these(dirs_and_files: list[str], dir: Path):
                 shutil.rmtree(file_path)
             else:
                 file_path.unlink()
+        else:
+            logger.debug(f"{file_path} does not exist, skipping deletion.")
