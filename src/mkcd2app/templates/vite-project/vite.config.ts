805214fa7c328e251159d74033f3a3311a1d24c2
Cyrus Yiu <UnsignedArduino@outlook.com>
2026-07-23 14:15:38 -0400

    Manually code split out binary.js and ---simulator.html for slightly better performance

--- src/mkcd2app/templates/vite-project/vite.config.ts
+++ src/mkcd2app/templates/vite-project/vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import react, {reactCompilerPreset} from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({presets: [reactCompilerPreset()]})],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "binary", // Will emit as dist/assets/game-binary-[hash].js
+              test: /binary\.js/, // Matches your compiled game binary path or file name
+            },
+            {
+              name: "simulator",
+              test: /---simulator\.html/,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
